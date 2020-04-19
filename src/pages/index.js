0c5ea54648f1f4d9488e0c59eef04fbd25989abc
Andrew McGoveran <andrew.mcgoveran@shopify.com>
2020-04-19 17:46:54 -0400

    Added pages and basic styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import {Link} from 'gatsby';
 
 import Layout from '../components/Layout';
-import Image from '../components/Image';
 import SEO from '../components/Seo';
+import Home from '../components/Home';
 
 function IndexPage() {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
-      <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
-        <Image />
-      </div>
-      <Link to="/about">About</Link>
+      <Home />
     </Layout>
   );
 }
